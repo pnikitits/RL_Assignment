b3e9680f6bfec5c253d988de5e98c254f95f915a
pnikitits <112090831+pnikitits@users.noreply.github.com>
2024-04-08 23:38:26 +0200

    cleanup

---
+++ Experiments.py
@@ -0,0 +1,95 @@
+from MonteCarloAgent import MonteCarloAgent , train_monte_carlo
+from SarsaLambdaAgent import SarsaLambdaAgent , train_sarsa
+
+import gymnasium as gym
+import text_flappy_bird_gym
+
+import matplotlib.pyplot as plt
+import os, sys
+import time
+
+
+
+def play_game(env, agent):
+    #obs = env.reset()
+    state = env.reset()[0]
+
+    while True:
+        action = agent.choose_action(state)
+        next_state , _ , done , _ , _ = env.step(action)
+        state = next_state
+
+        os.system("clear")
+        sys.stdout.write(env.render())
+        time.sleep(0.2)
+
+        if done:
+            break
+
+    env.close()
+
+
+
+
+def run_sarsa(play = False):
+    env = gym.make('TextFlappyBird-v0', height = 15, width = 20, pipe_gap = 4)
+
+    SarsaSettings = {
+        "alpha": 0.12,
+        "gamma": 0.99,
+        "lam": 0.9,
+        "epsilon": 0.1,
+        "epsilon_decay": 0.999,
+        "epsilon_min": 0.01
+    }
+
+    agent = SarsaLambdaAgent(env.action_space , SarsaSettings)
+
+    episodes = 10000
+    agent , rewards , l500_mean = train_sarsa(env, agent, episodes)
+
+    if play:
+        play_game(env, agent)
+    else:
+        plt.plot(rewards)
+        plt.xlabel("Episode")
+        plt.ylabel("Reward")
+        plt.title("Reward vs Episode")
+        plt.show()
+
+
+
+
+def run_monte_carlo(play = False):
+    env = gym.make('TextFlappyBird-v0', height = 15, width = 20, pipe_gap = 4)
+
+    MonteCarloSettings = {
+        "epsilon": 0.1,
+        "epsilon_decay": 0.999,
+        "epsilon_min": 0.01,
+        "gamma": 0.99,
+        "alpha": 0.1
+    }
+
+    agent = MonteCarloAgent(env.action_space , MonteCarloSettings)
+
+    episodes = 10000
+    agent , rewards , l500_mean = train_monte_carlo(env, agent, episodes)
+
+    if play:
+        play_game(env, agent)
+    else:
+        plt.plot(rewards)
+        plt.xlabel("Episode")
+        plt.ylabel("Reward")
+        plt.title("Reward vs Episode")
+        plt.show()
+
+
+
+
+if __name__ == "__main__":
+    # run_sarsa(play=False)
+    run_monte_carlo(play=False)
+    
+    
---
+++ SarsaLambdaAgent.py
@@ -0,0 +1,80 @@
+import numpy as np
+np.random.seed(11)
+from tqdm import tqdm
+
+
+
+class SarsaLambdaAgent:
+    def __init__(self, action_space, agent_settings):
+        self.action_space = action_space
+
+        self.alpha = agent_settings["alpha"]
+        self.gamma = agent_settings["gamma"]
+        self.lam = agent_settings["lam"]
+        self.epsilon = agent_settings["epsilon"]
+        self.epsilon_decay = agent_settings["epsilon_decay"]
+        self.epsilon_min = agent_settings["epsilon_min"]
+
+        self.Q = {}
+        self.E = {}
+
+
+    def choose_action(self, state):
+        if np.random.uniform(0, 1) < self.epsilon:
+            return self.action_space.sample()
+        else:
+            q_values = [self.get_Q(state, a) for a in range(self.action_space.n)]
+            return np.argmax(q_values)
+
+
+    def get_Q(self, state, action):
+        return self.Q.get((state, action), 0.0)
+
+
+    def update(self, state, action, reward, next_state, next_action, done):
+        sa_pair = (state, action)
+        # next_sa_pair = (next_state, next_action)
+        
+        q_current = self.get_Q(state, action)
+        q_next = self.get_Q(next_state, next_action) if not done else 0
+        
+        delta = reward + self.gamma * q_next - q_current
+        
+        self.E[sa_pair] = self.E.get(sa_pair, 0.0) + 1
+
+        for sa in self.E:
+            self.Q[sa] = self.Q.get(sa, 0.0) + self.alpha * delta * self.E[sa]
+            self.E[sa] *= self.gamma * self.lam
+
+        self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+
+
+
+
+
+def train_sarsa(env, agent, episodes):
+    rewards = []
+
+    for _ in tqdm(range(episodes)):
+        state = env.reset()[0]
+        action = agent.choose_action(state)
+        done = False
+
+        total_reward = 0
+
+        while not done:
+            next_state , _ , done , _ , _ = env.step(action)
+
+            y_diff = next_state[1]
+            reward = -abs(y_diff) / 15 # Reward based on vertical distance from the center
+
+            next_action = agent.choose_action(next_state)
+            agent.update(state, action, reward, next_state, next_action, done)
+            state, action = next_state, next_action
+            total_reward += reward
+
+        rewards.append(total_reward)
+
+    env.close()
+    l500_mean = np.mean(rewards[-500:])
+    return agent , rewards , l500_mean
